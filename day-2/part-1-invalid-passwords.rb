b7c89b02d0258f035dcfa890340431b3d35e930f
Karla Gardiner <kgardiner@uk-c02z23q3lvdm.uk.deloitte.com>
2020-12-03 00:22:00 +0000

    Completed day-2

---
+++ day-2/part-1-invalid-passwords.rb
@@ -0,0 +1,23 @@
+# file = File.open("part-1-passwords.txt")
+# file_data = file.readlines.map(&:chomp)
+# file.close
+# p file_data
+counter = 0
+
+file_data = File.foreach("part-1-passwords.txt") do |line| 
+    line = line.split(" ")
+    # p line
+    element = line[0].split("-")
+    # p element
+    # p line[1]
+    # p line[2]
+    character_count = line[2].count(line[1][0])
+    # p character_count
+    if (character_count < element[0].to_i || character_count > element[1].to_i)
+    else
+        p line
+        counter += 1
+        p counter
+        line.push(true)
+    end
+end
---
+++ day-2/part-2-invalid-passwords.rb
@@ -0,0 +1,20 @@
+# file = File.open("part-1-passwords.txt")
+# file_data = file.readlines.map(&:chomp)
+# file.close
+# p file_data
+counter = 0
+
+file_data = File.foreach("part-1-passwords.txt") do |line| 
+    line = line.split(" ")
+    element = line[0].split("-")
+    character_count = line[2].count(line[1][0])
+    p line[2][element[0].to_i]
+    first_pos = line[2][element[0].to_i - 1] == line[1][0]
+    sec_pos = line[2][element[1].to_i - 1] == line[1][0]
+    if (first_pos || sec_pos)
+        if (first_pos != sec_pos)
+            counter += 1
+        end
+    end
+    p counter
+end
