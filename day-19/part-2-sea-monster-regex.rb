c33b3837c1ff4288d56e3ed2b66d519a4b74ac71
Karla Gardiner <karla.gardiner@gmail.com>
2020-12-27 20:19:30 +0000

    Completed day-19

---
+++ day-19/part-2-sea-monster-regex.rb
@@ -0,0 +1,64 @@
+require_relative '../helper'
+include Helper
+
+code_data = File.read("day-19/code-data.txt")
+test_1_data = File.read("day-19/part-1-test-1-data.txt")
+test_2_data = File.read("day-19/part-1-test-2-data.txt")
+
+def sea_monster data
+    input_rules = {}
+    regex_rules = {}
+    rules, letters = data.split(/\n\n/)
+    rules = rules.split(/\n/)
+    letters = letters.split(/\n/)
+
+    rule_a = rules.select { |rule| rule.include?("a") }
+    rule_b = rules.select { |rule| rule.include?("b") }
+
+    regex_rules[rule_a[0].split(":")[0]] = "a"
+    regex_rules[rule_b[0].split(":")[0]] = "b"
+
+    rules.each do |rule|
+        next if rule.include?("a") || rule.include?("b")
+        rule_key, input_rule = rule.split(": ")
+        if rule_key == "8"
+            input_rules[rule_key] = {
+                str: "42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42",
+                dep: ["42"]
+            }
+        elsif rule_key == "11"
+            input_rules[rule_key] = {
+                str: "42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31",
+                dep: input_rule.scan(/\d+/).uniq
+            }
+        else
+            input_rules[rule_key] = {
+                str: input_rule,
+                dep: input_rule.scan(/\d+/).uniq
+            }
+        end
+    end
+
+    loop do
+        rules_to_regexify = input_rules.select { |id, rule| (rule[:dep] - regex_rules.keys).empty? }
+        rules_to_regexify.each do |id, rule|
+            regex_string = "("
+            rule_combinations = rule[:str].split(" | ").map(&:split)
+            regex_strings = rule_combinations.map do |rule|
+                regex_rule = rule.map {|num| regex_rules[num] }.join("")
+            end
+            regex_rules[id] = "(#{regex_strings.join("|")})"
+            input_rules.delete(id)
+        end
+        break if input_rules.empty?
+    end
+
+    rule_0 = /\A#{regex_rules["0"]}\z/
+    valid_letters = letters.count do |letter|
+        !!letter.match(rule_0)
+    end
+    p valid_letters
+
+end
+
+sea_monster(code_data)
